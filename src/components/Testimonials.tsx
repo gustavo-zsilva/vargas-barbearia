20d39950b4f4196e6aae64a3eb8d815d4cbc1c98
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-01-31 15:09:21 -0300

    Finished testimonials layout

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,19 +1,66 @@
 import { fetchWrapper } from "../utils/fetch";
 import { UserComment } from "./UserComment";
 
+type AllPlacesData = {
+    place_id: string,
+}
+
+type Review = {
+    author_name: string,
+    author_url: string,
+    rating: number,
+    relative_time_description: string,
+    text: string,
+}
+
+type TestimonialsData = {
+    reviews: Review[]
+}
+
 export async function Testimonials() {
 
-    const testimonialData = await fetchWrapper('/place/details')
+    const { results: allPlacesData } = await fetchWrapper<{ results: AllPlacesData[] }>(`/geocode`, {
+        address: 'Vargas+Barbeiro,576',
+    })
+
+    const placeId = allPlacesData[0].place_id
+
+    const { result: testimonialData } = await fetchWrapper<{ result: TestimonialsData }>('/place/details', {
+        place_id: placeId,
+        fields: ['reviews'],
+    })
+
+    const relevantTestimonials = testimonialData.reviews.splice(0, 3)
 
     return (
-        <section className="flex bg-yellow px-44 py-16 items-center justify-center flex-col">
-            <h1 className="text-lg relative before:content-[''] before:absolute before:left-1/10 before:h-0.5 before:w-10/12 before:bg-brown">
+        <section className="flex flex-col bg-yellow px-44 py-16 items-center justify-center gap-16">
+            <h1 className="
+                text-lg
+                relative
+                before:content-['']
+                before:absolute
+                before:left-0
+                before:h-0.5
+                before:w-[80px]
+                before:top-[-30%]
+                before:bg-brown
+                before:translate-x-1/2
+            ">
                 DEPOIMENTOS
             </h1>
-            <div className="flex gap-24 text-grey mt-16">
-                <UserComment />
-                <UserComment />
-                <UserComment />
+            <div className="grid grid-cols-3 gap-24 text-grey">
+                {relevantTestimonials.map(review => (
+                    <UserComment
+                        key={review.author_url}
+                        review={review}
+                    />
+                ))}
+            </div>
+            <div className="flex items-center gap-16 text-grey">
+                <button className="bg-yellow-light font-semibold rounded-sm py-4 px-6 shadow">
+                    Deixe o seu depoimento!
+                </button>
+                <a href="#" className="underline underline-offset-2">Saiba mais</a>
             </div>
         </section>
     )
--- src/components/UserComment.tsx
+++ src/components/UserComment.tsx
@@ -1,25 +1,51 @@
 import { MdOutlineStar } from "react-icons/md"
 import { BiSolidQuoteLeft } from "react-icons/bi"
 
-export function UserComment() {
+type Review = {
+    author_name: string,
+    author_url: string,
+    rating: number,
+    relative_time_description: string,
+    text: string,
+}
+
+type UserCommentProps = {
+    review: Review,
+}
+
+export function UserComment({ review }: UserCommentProps) {
+
+    const ratingArr = Array.from({ length: review.rating }, (value, index) => index) // [0, 1, 2, ...]
+    const authorSurname = review.author_name.split(' ')[1][0] + '.'
+    const formattedAuthorName = review.author_name.split(' ')[0] + ` ${authorSurname}`
+
     return (
-        <div className="flex flex-col bg-yellow-light p-7 gap-6 rounded-2xl">
+        <div className="flex flex-col bg-yellow-light p-7 gap-6 rounded-2xl shadow-sm">
             <header className="flex justify-between items-center gap-8">
                 <div className="flex">
-                    <MdOutlineStar size={26} color="#FFF" />
-                    <MdOutlineStar size={26} color="#FFF" />
-                    <MdOutlineStar size={26} color="#FFF" />
-                    <MdOutlineStar size={26} color="#FFF" />
-                    <MdOutlineStar size={26} color="#FFF" />
+                    {ratingArr.map(ratingNum => (
+                        <MdOutlineStar key={ratingNum} size={26} color="#FFF" />
+                    ))}
                 </div>
                 <span className="font-medium">
-                    Jean C.
+                    <a
+                        href={review.author_url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {formattedAuthorName}
+                    </a>
                 </span>
             </header>
-            <main className="flex">
-                <BiSolidQuoteLeft size={26} color="#FFF" className="min-w-fit" />
-                <p className="ml-2 leading-6">
-                    Sou cliente fiel desta barbearia há um tempo e posso dizer que adoro o trabalho do Vargas. O atendimento sempre é excelente. Sem dúvidas, recomendo.
+            <main className="flex flex-col justify-between h-full">
+                <div className="flex">
+                    <BiSolidQuoteLeft size={26} color="#FFF" className="min-w-fit" />
+                    <p className="flex flex-col h-full ml-[8px] leading-6">
+                        {review.text}
+                    </p>
+                </div>
+                <p className="mt-3 ml-[34px] opacity-80">
+                    {review.relative_time_description}
                 </p>
             </main>
         </div>
