d25fce5efcf8e8700f90312617350f3ebc9f065e
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-01-31 16:07:47 -0300

    finished contacts layout

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,13 +1,41 @@
 import { GoogleMapsEmbed } from '@next/third-parties/google'
+import { fetchWrapper } from '../utils/fetch'
 
-export function Contact() {
+type AllPlacesData = {
+    place_id: string,
+}
+
+type ContactData = {
+    formatted_phone_number: string,
+    formatted_address: string,
+    current_opening_hours: {
+        weekday_text: string[],
+    }
+}
+
+export async function Contact() {
     const apiKey = process.env.GOOGLE_API_KEY || ""
+
+    const { results: allPlacesData } = await fetchWrapper<{ results: AllPlacesData[] }>(`/geocode`, {
+        address: 'Vargas+Barbeiro,576',
+    })
+
+    const placeId = allPlacesData[0].place_id
+
+    const { result } = await fetchWrapper<{ result: ContactData }>(`/place/details`, {
+        place_id: placeId,
+        fields: ['formatted_phone_number', 'formatted_address', 'current_opening_hours/weekday_text']
+    })
+
+    console.log(result.current_opening_hours.weekday_text[0].search(':'));
+    
 
     return (
         <section className="flex flex-col justify-center items-center mx-44 py-16">
             <h1 className="
                 text-lg
                 relative
+                mb-16
                 before:content-['']
                 before:absolute
                 before:left-0
@@ -19,17 +47,45 @@ export function Contact() {
             ">
                 ENTRE EM CONTATO
             </h1>
-            <div className="columns-2 w-full gap-20">
-                <div className="">
+            <div className="grid grid-cols-2 w-full gap-20">
+                <div>
                     <GoogleMapsEmbed
                         apiKey={apiKey}
                         height={400}
                         width="100%"
                         mode="place"
                         q="Vargas+Barbeiro,576"
+                        style="box-shadow: 0 1px 2px 0 rgb(0 0 0 / 0.05);"
                     />
+                    <button className="uppercase text-grey font-bold bg-yellow w-full py-5 mt-10 rounded-sm shadow">
+                        Ver Rotas
+                    </button>
                 </div>
-                <div className="border-2 border-red">3</div>
+                <div className="flex flex-col gap-8">
+                    <div>
+                        <h2 className="text-base mb-3">Contato:</h2>
+                        <p>{result.formatted_phone_number}</p>
+                        <p>{result.formatted_phone_number}</p>
+                    </div>
+                    <div>
+                        <h2 className="text-base mb-3">Endereço:</h2>
+                        <p>{result.formatted_address}</p>
+                    </div>
+                    <div>
+                        <h2 className="text-base mb-3">Horários de funcionamento:</h2>
+                        {result.current_opening_hours.weekday_text.map(weekday => {
+                            const shortWeekday = weekday.split(' ')[0].slice(0,3)
+                            const schedule = weekday.split(' ')[1]
+                            
+                            return (
+                                <p className="flex">
+                                    <span className="w-14">{`${shortWeekday}.:`}</span>
+                                    <span>{schedule}</span>
+                                </p>
+                            )
+                        })}
+                    </div>
+                </div>
             </div>
         </section>
     )
