2bb4d09e7756871334ed1f81aead9a18c3b8ef33
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-02-01 11:28:50 -0300

    dynamic phone number

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,34 @@
 import { kenia } from '../fonts'
-import { FaPhone } from 'react-icons/fa6'
 
+import { FaPhone } from 'react-icons/fa6'
 import { WhatsappButton } from './WhatsappButton'
 
-export function Header() {
+import { fetchWrapper } from '../utils/fetch'
+
+type Result = {
+    formatted_phone_number: string,
+}
+
+export async function Header() {
+
+    const { result } = await fetchWrapper<{ result: Result }>('/place/details', {
+        fields: ['formatted_phone_number']
+    })
+    const phoneNumber = result.formatted_phone_number
+
     return (
         <header className="bg-yellow py-4 flex items-center justify-between px-44 shadow-md">
-            <h1 className={`text-xl ${kenia.className}`}>Vargas Barbearia</h1>
+            <h1 className={`text-xl ${kenia.className} cursor-pointer`}>Vargas Barbearia</h1>
             <div className="flex gap-8">
                 <p className="flex gap-2.5 self-center">
                     Atendimento:
-                    <a href="#" className="flex items-center gap-2.5">
+                    <a
+                        href={`tel:${phoneNumber}`}
+                        target="_blank"
+                        className="flex items-center gap-2.5"
+                    >
                         <FaPhone />
-                        (xx) xxxx-xxx
+                        {phoneNumber}
                     </a>
                 </p>
                 <button className="bg-brown px-6 py-2 text-grey font-bold rounded-sm shadow-sm">
