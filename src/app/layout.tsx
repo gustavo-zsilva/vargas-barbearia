e64c2f6be073723941cfa526a210eaf50d461bf6
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-02-01 11:26:53 -0300

    added smooth scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth motion-reduce:scroll-auto">
       <body className={rubik.className}>{children}</body>
     </html>
   );
