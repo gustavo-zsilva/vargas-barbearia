7785cc9041437df43f380e737839acd4eaf4a2e6
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-02-01 11:29:32 -0300

    replaced button with link

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -2,8 +2,8 @@ import { FaWhatsapp } from "react-icons/fa6"
 
 export function WhatsappButton() {
     return (
-        <button className="bg-green p-2 rounded-sm shadow-sm">
+        <a className="bg-green rounded-sm shadow-sm p-2" href="">
             <FaWhatsapp size={28} color="#F8F7FF" />
-        </button>
+        </a>
     )
 }
