6a22a4c06b9fad0480ee4c9b01c5e6408deaad4a
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-02-01 11:27:28 -0300

    added Footer & removed redundant styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,15 @@ import { Menu } from "../components/Menu";
 import { Gallery } from "../components/Gallery";
 import { Testimonials } from "../components/Testimonials";
 import { Contact } from "../components/Contact";
+import { Footer } from "../components/Footer";
 
 export default function Home() {
   return (
     <main className="bg-grey text-brown h-full">
       <Header />
-      <div className="flex mx-44 py-16 text-base gap-16">
+      <div className="flex mx-desktop py-16 text-base gap-16">
         <div className="flex flex-col w-5/12 gap-10">
-          <p className="flex text-lg">
+          <p className="text-lg">
             Cortes de cabelo & barba cinco estrelas, feitos há mais de 25 anos no mercado.
           </p>
           <Location />
@@ -20,13 +21,14 @@ export default function Home() {
           <Menu />
         </div>
         
-        <div className="flex flex-1">
+        <div className="flex-1">
           <Gallery />
         </div>
       </div>
 
       <Testimonials />
       <Contact />
+      <Footer />
     </main>
   );
 }
