f05142ec840cbbeeb3e5fc9374bf44fc1598e352
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-02-08 13:09:43 -0300

    added external link

--- src/components/WhatsappButton.tsx
+++ src/components/WhatsappButton.tsx
@@ -2,7 +2,11 @@ import { FaWhatsapp } from "react-icons/fa6"
 
 export function WhatsappButton() {
     return (
-        <a className="bg-green rounded-sm shadow-sm p-2" href="">
+        <a
+            href="https://www.whatsapp.com/product/4329414817136290/554898224490/?app_absent=0"
+            target="_blank"
+            className="bg-green rounded-sm shadow-sm p-2"
+        >
             <FaWhatsapp size={28} color="#F8F7FF" />
         </a>
     )
