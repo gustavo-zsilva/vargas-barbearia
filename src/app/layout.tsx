bbe44e37b8340a7e10eaddfd61a4f818a71c2d36
Gustavo Zonta <gustavozontadasilva@gmail.com>
2024-02-08 19:12:41 -0300

    changed lang to pt-br

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth motion-reduce:scroll-auto">
+    <html lang="pt-BR" className="scroll-smooth motion-reduce:scroll-auto">
       <body className={rubik.className}>{children}</body>
     </html>
   );
